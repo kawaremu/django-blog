df9c6aa470f84298cf08cda56c14b6236e3d0554
Ahlem <boucah@hotmail.com>
2022-03-10 11:38:00 +0100

    fixed README

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Every table is represented by the class Model in Django.
+# in the models module you can find
+class Post(models.Model):
+  title = models.CharField(max_length=100)
+  content = models.TextField() #unrestricted
+  date_posted = models.DateTimeField(default=timezone.now)
+  # The ForeignKey class means that : if User (author) is deleted -> their posts are deleted
+  author = models.ForeignKey(User,on_delete=models.CASCADE)
+  
